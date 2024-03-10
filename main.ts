e3dd1d301ed28b86e7cac0759cc6025ed77ceb99
MR BUNTY <MRxBunty>
2024-03-10 04:33:55 -0600

    project 2

---
+++ main.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+console.log(chalk.yellowBright("HELLLLO, ") + chalk.blueBright("FREIND,s"));
+console.log(chalk.greenBright.italic("MY NAME IS RAHEEL,"))
+
+
+async function main() {
+    let answers = await inquirer.prompt([
+        {
+            name: "age",
+            type: "number",
+            message: "Enter your age"
+        }
+    ]);
+
+    console.log(chalk.green(`Insha Allah, in ${60 - answers.age} years you will be 60 years old.`));
+}
+
+main();
